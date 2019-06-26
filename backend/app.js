290eff677c9626325e3aa2ebfa9e2d418b6926a5
hmallen99 <31718268+hmallen99@users.noreply.github.com>
2019-06-25 22:37:52 -0700

    added tables

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,34 @@
 const http = require('http');
 var mysql = require('mysql');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+var text = fs.readFileSync('database_info.txt');
+var credentials = text.toString().split("\r\n");
+
 var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: ""
+    host: credentials[0],
+    user: credentials[1],
+    password: credentials[2],
+    database: credentials[3]
 });
+
+//tables: (also in databse_info.txt)
+//albums
+//artists
 
 con.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!")
+    console.log("Connected!");
+    var sql = `
+        SELECT * FROM artists;
+    `;
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    })
 });
 
 const server = http.createServer((req, res) => {
